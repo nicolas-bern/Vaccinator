225b271ccc28028d8b53fe203a034e2bddf7284a
nicolas.bern <nicolas.bern@epsi.fr>
2021-04-09 16:37:19 +0200

    Personnes non vaccinées en fonction d'un vaccin

--- Vaccinator/Controllers/PersonnesNonVaccineesController.cs
+++ Vaccinator/Controllers/PersonnesNonVaccineesController.cs
@@ -19,140 +19,20 @@ namespace Vaccinator.Controllers
         }
 
         // GET: PersonnesNonVaccinees
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(String VaccinSelect)
         {
             ViewBag.ListVaccins = _context.Vaccin.Select(v => new SelectListItem { Text = v.Type, Value = v.Type }).ToList();
 
             IQueryable<Personne> listPersonnes;
 
-            listPersonnes = from p in _context.Personnes where !(from i in _context.Injection select i.Personne.Id).Contains(p.Id) select p;
+            listPersonnes = from p in _context.Personnes where !(
+                            from i in _context.Injection where (
+                            from v in _context.Vaccin where v.Type.Contains(VaccinSelect) select v.Type).Contains(i.Vaccin.Type)
+                            select i.Personne.Id).Contains(p.Id) select p;
 
             return View(await listPersonnes.ToListAsync());
         }
 
-        // GET: PersonnesNonVaccinees/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var personne = await _context.Personnes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (personne == null)
-            {
-                return NotFound();
-            }
-
-            return View(personne);
-        }
-
-        // GET: PersonnesNonVaccinees/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: PersonnesNonVaccinees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nom,Prenom,Sexe,DateNaissance,IsResident")] Personne personne)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(personne);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(personne);
-        }
-
-        // GET: PersonnesNonVaccinees/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var personne = await _context.Personnes.FindAsync(id);
-            if (personne == null)
-            {
-                return NotFound();
-            }
-            return View(personne);
-        }
-
-        // POST: PersonnesNonVaccinees/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nom,Prenom,Sexe,DateNaissance,IsResident")] Personne personne)
-        {
-            if (id != personne.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(personne);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PersonneExists(personne.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(personne);
-        }
-
-        // GET: PersonnesNonVaccinees/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var personne = await _context.Personnes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (personne == null)
-            {
-                return NotFound();
-            }
-
-            return View(personne);
-        }
-
-        // POST: PersonnesNonVaccinees/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var personne = await _context.Personnes.FindAsync(id);
-            _context.Personnes.Remove(personne);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PersonneExists(int id)
-        {
-            return _context.Personnes.Any(e => e.Id == id);
-        }
+        
     }
 }
